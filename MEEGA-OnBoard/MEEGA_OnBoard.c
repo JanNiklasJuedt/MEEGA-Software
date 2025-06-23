b1c7d6cffc283d905dfdde993695f27139b52f17
JanNiklasJuedt <jan-niklas.juedt@alumni.fh-aachen.de>
2025-06-23 19:44:57 +0200

    first onboard/datahandling files

---
+++ MEEGA-OnBoard/MEEGA_OnBoard.c
@@ -0,0 +1,20 @@
+#include <math.h>
+
+int main() {
+	//Here comes the money
+	return 0;
+}
+
+struct params {
+	int Mode;
+};
+
+int ExperimentRun(struct params parameter) {
+	//...
+	int EoE = 1;
+	return 0;
+}
+
+void DataAquisition() {
+	return;
+}
